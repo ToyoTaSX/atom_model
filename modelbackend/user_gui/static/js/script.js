55ab9ef31cc4bfceffe48dcd1d92772517dbb8d7
Daniil-Ka <dkrapivin.04@gmail.com>
2024-06-15 16:22:33 +0500

    js скрипт, для отображения

--- modelbackend/user_gui/static/js/script.js
+++ modelbackend/user_gui/static/js/script.js
@@ -1,16 +1,30 @@
 document.getElementById('photo-input').addEventListener('change', function(event) {
-    const preview = document.getElementById('preview');
-    preview.innerHTML = ''; // Clear previous previews
     const files = event.target.files;
     if (files) {
-        Array.from(files).forEach(file => {
-            const reader = new FileReader();
-            reader.onload = function(e) {
-                const img = document.createElement('img');
-                img.src = e.target.result;
-                preview.appendChild(img);
+        const formData = new FormData();
+        formData.append('image', files[0]);
+
+        fetch('/api/get_photo_class/', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.image_base64) {
+                // Создаем элемент img для отображения обработанного изображения
+                const imgElement = document.createElement('img');
+                imgElement.src = 'data:image/png;base64,' + data.image_base64;
+
+                // Очищаем предыдущие превью и добавляем новое изображение
+                const preview = document.getElementById('preview');
+                preview.innerHTML = '';
+                preview.appendChild(imgElement);
+            } else {
+                console.error('Ответ сервера не содержит image_base64');
             }
-            reader.readAsDataURL(file);
+        })
+        .catch(error => {
+            console.error('Ошибка при отправке или получении изображения:', error);
         });
     }
 });
